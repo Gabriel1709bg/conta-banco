629d12739ceb5f7eed141dd1de9dc5470f627a53
Gabriel <glima6497@gmail.com>
2023-09-19 12:33:32 -0300

    Projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+//TODO:"Conhecer e importar a classe Scanner"
+// "Exibir as mensagens para o nosso usuário"
+// "Obter pelo Scanner os valores digitados no terminal"
+// "Exibir a mensagem conta criada"
+
+
+        System.out.println("Parabéns, a sua conta foi criada!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o valor que deseja depositar: ");
+        double saldo = scanner.nextDouble();
+
+
+        System.out.println("Digite o valor que deseja sacar: ");
+        double valorSolicitado = scanner.nextDouble();
+
+
+
+        if (valorSolicitado < saldo) {
+            double saldoAtual = saldo - valorSolicitado;
+            System.out.println("Você sacou, agora seu saldo é: " + saldoAtual);
+        }
+        if (valorSolicitado > saldo) {
+            System.out.println("Seu saldo é: " + saldo + "\nNão foi possível sacar, seu saldo é insuficiente.");
+        }
+
+
     }
 }
